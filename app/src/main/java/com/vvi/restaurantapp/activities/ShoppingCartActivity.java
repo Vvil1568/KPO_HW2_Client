401aee814ecf2ef881c9cb5c50e8baa8269ac28f
Goodbird <goodbirdmail@yandex.ru>
2024-02-21 20:37:25 +0300

    Added different layouts

---
+++ app/src/main/java/com/vvi/restaurantapp/activities/ShoppingCartActivity.java
@@ -0,0 +1,16 @@
+package com.vvi.restaurantapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvi.restaurantapp.R;
+
+public class ShoppingCartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_cart);
+    }
+}
---
+++ app/src/main/java/com/vvi/restaurantapp/activities/UserMainActivity.java
@@ -0,0 +1,16 @@
+package com.vvi.restaurantapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vvi.restaurantapp.R;
+
+public class UserMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main);
+    }
+}
