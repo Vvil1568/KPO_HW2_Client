18e2ee6d6d95a70d85595831cbe04f93a6435e45
Goodbird <goodbirdmail@yandex.ru>
2024-02-23 15:47:03 +0300

    Added resources for user management and user class

---
+++ app/src/main/java/com/vvi/restaurantapp/items/User.java
@@ -0,0 +1,29 @@
+package com.vvi.restaurantapp.items;
+
+public class User {
+    private final String fio;
+    private final String login;
+    private boolean isAdmin;
+
+    public User(String fio, String login, boolean isAdmin) {
+        this.fio = fio;
+        this.login = login;
+        this.isAdmin = isAdmin;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void changeMode(){
+        isAdmin = !isAdmin;
+    }
+}
